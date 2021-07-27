d6603daea7c591cbb3e25ab8a70d2784659a905d
Avin Sheri Joseph <avinsheri65@gmail.com>
2021-07-27 19:40:52 +0530

    ElasticSearch - CustomQuery

--- src/main/java/com/sayone/ebazzar/controller/SearchController.java
+++ src/main/java/com/sayone/ebazzar/controller/SearchController.java
@@ -4,11 +4,15 @@ import com.sayone.ebazzar.document.ElasticProduct;
 import com.sayone.ebazzar.service.SearchServices;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
+import org.elasticsearch.search.SearchHit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @ApiImplicitParams({@ApiImplicitParam(name = "authorization", value = "${userController.authorizationHeader.description}", paramType = "header")})
@@ -18,11 +22,11 @@ public class SearchController {
     SearchServices searchServices;
 
     @GetMapping("/name")
-    public ElasticProduct addProductByName(@RequestParam String name){
+    public List<ElasticProduct> addProductByName(@RequestParam String name){
         return searchServices.getProductByName(name);
     }
     @GetMapping("/subcategory")
-    public ElasticProduct addProductBySubcategory(@RequestParam String name){
+    public List<ElasticProduct> addProductBySubcategory(@RequestParam String name){
         return searchServices.getProdutBySubcategory(name);
     }
 }
--- src/main/java/com/sayone/ebazzar/service/SearchServices.java
+++ src/main/java/com/sayone/ebazzar/service/SearchServices.java
@@ -2,8 +2,22 @@ package com.sayone.ebazzar.service;
 
 import com.sayone.ebazzar.document.ElasticProduct;
 import com.sayone.ebazzar.repository.ProductElasticRepository;
+
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.Operator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
 
 @Service
 public class SearchServices {
@@ -11,11 +25,46 @@ public class SearchServices {
     @Autowired
     ProductElasticRepository productElasticRepository;
 
-    public ElasticProduct getProductByName(String name) {
-        return productElasticRepository.findByName(name);
+    @Autowired
+    ElasticsearchRestTemplate elasticsearchTemplate;
+
+    public List<ElasticProduct> getProductByName(String name) {
+        String data=String.format(".*%s.*",name);
+        NativeSearchQuery searchQuery1 = new NativeSearchQueryBuilder()
+                .withFilter(regexpQuery("name", data))
+                .build();
+
+        SearchHits<ElasticProduct> products = elasticsearchTemplate
+                .search(searchQuery1,ElasticProduct.class,IndexCoordinates.of("product"));
+
+        List<ElasticProduct> returnValue = new ArrayList<ElasticProduct>();
+
+        products.getSearchHits().forEach(searchHit->{
+            returnValue.add(searchHit.getContent());
+        });
+
+        return returnValue;
     }
-    public ElasticProduct getProdutBySubcategory(String name){
-        return productElasticRepository.findBysubcategory(name);
+
+
+    public List<ElasticProduct> getProdutBySubcategory(String name){
+        String data=String.format(".*%s.*",name);
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(matchQuery("subcategory", data).operator(Operator.AND)
+                .fuzziness(Fuzziness.AUTO)
+                .prefixLength(3))
+                .build();
+
+        SearchHits<ElasticProduct> products = elasticsearchTemplate
+                .search(searchQuery,ElasticProduct.class,IndexCoordinates.of("product"));
+
+        List<ElasticProduct> returnValue = new ArrayList<ElasticProduct>();
+
+        products.getSearchHits().forEach(searchHit->{
+            returnValue.add(searchHit.getContent());
+        });
+
+        return returnValue;
     }
 
 }
